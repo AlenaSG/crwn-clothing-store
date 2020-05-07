1b78061016c05ebcc84b7d89cc4d67744a722f9e
Alena Golovina <alenasgolovina@gmail.com>
2020-05-06 20:13:50 -0700

    added custom input and button components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,6 +15,9 @@ const Header = () => (
             <Link className='option' to='/conact'>
                 CONTACT
             </Link>
+            <Link className='option' to='/signin'>
+                SIGN IN
+            </Link>
         </div>
     </div>
 )

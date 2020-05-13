7316f65a1d7e7a6a2f8e60f127954854cf7e4031
Alena Golovina <alenasgolovina@gmail.com>
2020-05-13 15:35:40 -0700

    implemented firebaser utils, included ability to store authenticated users into the firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInUp from './pages/sign-in-up-page/sign-in-up-page.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -20,10 +20,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      }
 
-      console.log(user);
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/pages/sign-in-up-page/sign-in-up-page.component.jsx
+++ src/pages/sign-in-up-page/sign-in-up-page.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './sign-in-up-page.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInUp = () => (
     <div className="sign-in-up">
         <SignIn />
+        <SignUp />
     </div>
 );
 
